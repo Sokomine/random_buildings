d00fbd1eca148089351818161227953a757b124d
Sokomine <wegwerf@anarres.dyndns.org>
2014-10-09 03:30:42 +0200

    added wool for tents

--- cottages/nodes_historic.lua
+++ cottages/nodes_historic.lua
@@ -175,6 +175,34 @@ minetest.register_node("cottages:wood_flat", {
 })
 
 ---------------------------------------------------------------------------------------
+-- useful for building tents
+---------------------------------------------------------------------------------------
+minetest.register_node("cottages:wool_tent", {
+		description = S("wool for tents"),
+		drawtype = "nodebox",
+                -- top, bottom, side1, side2, inner, outer
+		tiles = {"wool_white.png"},
+		paramtype = "light",
+		paramtype2 = "facedir",
+		groups = {snappy=2,choppy=2,oddly_breakable_by_hand=2},
+		node_box = {
+			type = "fixed",
+			fixed = {
+				{ -0.5, -0.5, -0.50,  0.5, -0.5+1/16, 0.50},
+			},
+		},
+		selection_box = {
+			type = "fixed",
+			fixed = {
+				{ -0.5, -0.5, -0.50,  0.5, -0.5+1/16, 0.50},
+			},
+		},
+		is_ground_content = false,
+		on_place = minetest.rotate_node,
+})
+
+
+---------------------------------------------------------------------------------------
 -- crafting receipes
 ---------------------------------------------------------------------------------------
 minetest.register_craft({
